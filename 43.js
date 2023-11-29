49de000481b7bfc60d61a80d304a0c65c076fc79
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-29 20:36:59 +0500

    43

---
+++ 43.js
@@ -0,0 +1,10 @@
+const arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14]
+];
+
+const subarray = arr[3];
+const nestedSubarray = arr[3][0];
+const element = arr[3][0][1];
