c4d0ba6e0f3d372454bb476e5e4cfce4fe0f5a07
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-29 20:35:21 +0500

    41

---
+++ 41.js
@@ -0,0 +1,5 @@
+const arr = ["Programming", 123, "Coding", 789];
+
+const firstElem = arr[0]; // This is "Programming"
+const thirdElem = arr[2]; // This is "Coding"
+const fourthElem = arr[3]; // This is 789
