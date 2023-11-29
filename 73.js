b62c3078417976ad8960ebac6bfaa4802d536735
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-29 23:42:17 +0500

    73

---
+++ 73.js
@@ -0,0 +1,22 @@
+/*Создайте объект calculator (калькулятор) с тремя методами:
+
+read() (читать) запрашивает два значения и сохраняет их как свойства объекта с именами a и b.
+sum() (суммировать) возвращает сумму сохранённых значений.
+mul() (умножить) перемножает сохранённые значения и возвращает результат.*/
+
+const calculator = {
+    read(a, b){
+        this.a = a;
+        this.b = b;
+    },
+    sum(){
+        return this.a + this.b;
+    },
+    mul(){
+        return this.a * this.b;
+    },
+};
+
+calculator.read(54, 10);
+console.log(calculator.sum());
+console.log(calculator.mul());
