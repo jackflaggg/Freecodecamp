ab6e5b005363a4990b8d903f5bc88160e5e8d011
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-12-03 01:08:44 +0500

    87

---
+++ 87.js
@@ -0,0 +1,17 @@
+function getMaxSubSum(arr) {
+    let currentSum = arr[0];
+    let maxSum = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (currentSum + arr[i] >= arr[i]) {
+            currentSum += arr[i];
+        } else {
+            currentSum = arr[i];
+        }
+        if (currentSum > maxSum) {
+            maxSum = currentSum;
+        }
+    }
+        return console.log(maxSum > 0 ? maxSum : 0);
+}
+
+getMaxSubSum([-2, -8, -5, -1, -2, -3, -2])
