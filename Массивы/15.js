3bc2cc1fdfbf5a09dabe4d87efbc8db1daa9c5d1
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-12-05 23:53:57 +0500

    massiv

--- Массивы/15.js
+++ Массивы/15.js
@@ -1,3 +1,17 @@
 /*Напишите функцию sortByAge(users), которая принимает массив объектов со свойством age и сортирует их по нему.*/
-//
-//
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let arr = [ vasya, petya, masha ];
+
+function sortByAge(users){
+    return users.sort((itemOne, itemTwo) => itemOne.age > itemTwo.age ? 1 : -1)
+}
+
+sortByAge(arr);
+
+console.log(arr[0].name); // Вася
+console.log(arr[1].name); // Маша
+console.log(arr[2].name); // Петя
+
