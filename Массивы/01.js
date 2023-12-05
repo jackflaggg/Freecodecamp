5dc5b86f2338e5330a8aaa6fbca5ca31e77dee87
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-12-05 17:42:11 +0500

    massiv

--- Массивы/01.js
+++ Массивы/01.js
@@ -9,13 +9,15 @@ camelize("list-style-image") == 'listStyleImage';
 camelize("-webkit-transition") == 'WebkitTransition';
 P.S. Подсказка: используйте split, чтобы разбить строку на массив символов, потом переделайте всё как нужно и методом join соедините обратно.*/
 
-function camelize(str){
+function camelize(str) {
     let splitPerem = str.split('-');
     let modifiedArray = splitPerem.map(item => {
-        if (typeof item === 'string' && item.length > 0){
-
+        if (typeof item === 'string' && item && item !== splitPerem[0][0]) {
+            return item[0].toUpperCase() + item.substring(1, item.length);
         }
-    })
+    });
+
+    return modifiedArray.join('');
 }
 
 console.log(camelize('background-color'));
--- Массивы/02.js
+++ Массивы/02.js
@@ -1,7 +1,7 @@
 let arr = "-webkit-transition";
-let splitStr = arr.split('-');
-let modifiedArr = splitStr.map(item => {
-    return (typeof item === 'string' && item.length > 0 && item !== splitStr[0]) ? item[0].toUpperCase() + item.substring(1, item.length) : item;
+
+let modifiedArr = arr.split('-').map(item => {
+    return (typeof item === 'string' && item && item !== arr.split('-')[0]) ? item[0].toUpperCase() + item.substring(1, item.length) : item;
 }).join('');
 
-console.log(modifiedArr);
+console.log(modifiedArr)
